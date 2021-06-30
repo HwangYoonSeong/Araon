3ce67d25568bac1c858b0b0b064808adc165ef20
yooncastle <dbstjd427@gmail.com>
2021-06-30 12:49:55 +0900

    RN+GoogleMaps

--- Araon_Front/App.js
+++ Araon_Front/App.js
@@ -1,68 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { SafeAreaView, StyleSheet, TextInput, View, Text, ScrollView } from 'react-native';
-import TodoInsert from './TodoComp/TodoInsert';
-import TodoList from './TodoComp/TodoList';
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+
 export default function App () {
-  const [todos, setTodos] = useState([]);
-
-  const onToggle = id => e => {
-    setTodos(
-      todos.map(todo =>
-        todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-      ),
+    return (
+        <View style={styles.container}>
+            <MapView style={styles.map} />
+        </View>
     );
-  };
-
-  const onRemove = id => e => {
-    setTodos(todos.filter(todo => todo.id !== id));
-  };
-  const addTodo = text => {
-    setTodos([
-      ...todos,
-      { id: Math.random().toString(), textValue: text, checked: false },
-    ]);
-  };
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.appTitle}>Hello Todolist</Text>
-      <View style={styles.card}>
-        <TodoInsert onAddTodo={addTodo} />
-        <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
-      </View>
-    </SafeAreaView>
-  );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#3143e8',
-
-  },
-  appTitle: {
-    color: '#fff',
-    fontSize: 36,
-    marginTop: 30,
-    marginBottom: 30,
-    fontWeight: '300',
-    textAlign: 'center',
-    backgroundColor: '#3143e8',
-  },
-  card: {
-    backgroundColor: '#fff',
-    flex: 1,
-    borderTopLeftRadius: 10, // to provide rounded corners
-    borderTopRightRadius: 10, // to provide rounded corners
-    marginLeft: 10,
-    marginRight: 10,
-  },
-  input: {
-    padding: 20,
-    borderBottomColor: '#bbb',
-    borderBottomWidth: 1,
-    fontSize: 24,
-    marginLeft: 20,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    map: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    },
 });
