6a8fd67ec4d06ca6dd80ea90104ac856b3b4d9e9
yooncastle <dbstjd427@gmail.com>
2021-08-11 14:03:33 +0900

    Set layout

--- araon_carouselpage/src/App.js
+++ araon_carouselpage/src/App.js
@@ -1,81 +1,135 @@
 import styled from 'styled-components';
-import Slider from "react-slick";
+import React, { useRef } from 'react';
 
-import { ChevronBackOutline } from 'react-ionicons'
-import { ChevronForwardOutline } from 'react-ionicons'
+import pizz from './assets/pizz.jpg';
+import pZed from './assets/Proj_Zed.jpg';
+import akali from './assets/akali.jpg';
+import sZed from './assets/Zed.png';
 
-const Slick = styled.div`
-    margin:0 auto;
-    width: 400px;
-    background-color:blue;   
-`;
+const Container = styled.div`
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  background-color: #343a40;
+  height:90vh;
+`
+
+const Carousel = styled.div`
+`
+
+
+const Wrapper = styled.div`
+width: 90vw; 
+height: 80vh;
+overflow: hidden;
+background-color:red;
+ &>${Carousel}{
+  display: inline-flex;
+  transform: translate3d(0, 0, 0); 
+  transition: transform 0.4s;
+ }   
+`
+// const Content = styled.div`
+//   display:flex;
+//   width:500px;
+//   height:300px;
+//   justify-content:center;
+//   align-items:center;
+
+//   ${(props) => {
+//     switch (props.color) {
+//       case 0:
+//         return css`
+//          background-color:#74c0fc;
+//          `
+//       case 1:
+//         return css`
+//          background-color:#8ce99a;
+//          `
+//       case 2:
+//         return css`
+//          background-color:#ffe066;
+//          `
+//       default:
+//         return css`
+//          background-color:#c0eb75;
+//          `
+
+//     }
+//   }
+
+//   }
+
+// `
 const Content = styled.div`
-    background-color:green;   
-`;
-const El = styled.div`
-    margin:0 auto;
-    background-color:red;   
-    width:300px;
-    height: 300px;
-    text-align:center;
-    color:white;
-    font-size:30px;
-`;
-
-function SamplePrevArrow (props) {
-  const { onClick } = props;
-  return (
-    <div
-      onClick={onClick}
-      style={{ backgroundColor: 'yellow', float: 'left', marginTop: '150px', marginLeft: '-100px' }}
-    ><ChevronBackOutline
-        color={'black'}
-        height="50px"
-        width="50px"
-      /></div>
-
-  );
-}
-function SampleNextArrow (props) {
-  const { onClick } = props;
-  return (
-    <div
-      onClick={onClick}
-      style={{ backgroundColor: 'yellow', float: 'right', marginTop: '-150px', marginRight: '-100px' }}
-    ><ChevronForwardOutline
-        color={'#00000'}
-        height="50px"
-        width="50px"
-      /></div>
-  );
-}
+display:flex;
+justify-content:center;
+align-items:center;
+background-color:black;
+width: 90vw; 
+height: 80vh;
+`
+const Img = styled.img`
+  width: auto;
+  height: 80vh;
+  object-fit:cover;
+`
+const PrevBtn = styled.button`
+  margin:10px;
+`
+const NextBtn = styled.button`
+  margin:10px;
+`
+const Nav = styled.div`
+ background-color: black;
+ width:100%;
+ height:10vh;
+`
 function App () {
-  const settings = {
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    slidesToScroll: 1,
-    nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />
-  };
+  // const contents = [1, 2, 3, 4];
+  const carousel = useRef();
+  let index = 0;
+  const prev = () => {
+    if (index === 0) return;
+    index -= 1;
+    carousel.current.style.transform = `translate3d(-${90 * index}vw, 0, 0)`;
 
-  const contents = [1, 2, 3, 4, 5];
+  }
+
+  const next = () => {
+    if (index === 3) return;
+    index += 1;
+    carousel.current.style.transform = `translate3d(-${90 * index}vw, 0, 0)`;
+  }
   return (
     <div className="App">
-      <Slick>
-        <h2> Single Item</h2>
-        <Slider {...settings}>
-          {contents.map((el, idx) => {
-            return (
-              <Content key={idx}>
-                <El>{el}</El>
-              </Content>
-            );
-          })}
-        </Slider>
-      </Slick>
+      <Container>
+        <PrevBtn onClick={prev}>prev</PrevBtn>
+        <Wrapper>
+          <Carousel ref={carousel}>
+            {/* {
+            contents.map((el, idx) => {
+              return (
+                <Content key={idx} color={idx} ></Content>
+              )
+            })
+          } */}
+            <Content> <Img src={pizz}></Img></Content>
+            <Content> <Img src={sZed}></Img></Content>
+            <Content> <Img src={akali}></Img></Content>
+            <Content><Img src={pZed}></Img></Content>
+
+
+
+
+
+          </Carousel>
+        </Wrapper>
+
+        <NextBtn onClick={next}>next</NextBtn>
+      </Container>
+
+      <Nav></Nav>
     </div>
   );
 }
